c08232ce1e6485aa4c4ed0f830d50e49431fb9d8
uros-5 <uros.mrkobrada99@gmail.com>
2020-04-09 22:40:45 +0100

    dodate slike za knjige, promenjen template za url slike, promenjena tabela za stavke narudzbine

--- Server/settings.py
+++ Server/settings.py
@@ -62,6 +62,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'templates/static'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
