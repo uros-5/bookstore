72978f0112a03738bfd17981114abe8fe5262e17
uros-5 <uros.mrkobrada99@gmail.com>
2020-07-04 02:12:04 +0100

    implementirano ocenjivanje knjiga, obrisan package knjige urls i sve smesteno u jedan radi preglednosti,

--- TaskRestAPI/knjige_urls/knjige_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.urls import include
-from django.conf.urls import url
-from TaskRestAPI.views import knjiga_look
-urlpatterns = [
-    path(r'^(?P<br>[\d]+)$',knjiga_look,name="test")
-]
