e2e50683c2f70a7a01fe8c19105062986772865e
uros-5 <uros.mrkobrada99@gmail.com>
2020-07-04 15:27:50 +0100

    ispravljene greske sa drugim serverom u vezi banke

---
+++ ServerMobilnoBankaPosta.bat
@@ -0,0 +1,2 @@
+py.exe "D:/DL TB/web-programiranje-projektni-uros-mrkobrada/TaskRestAPI/miniAppsZaKnjizaru/ServerMobilnoBankaPosta/ServerMobilnoBankaPosta.py"
+pause
--- TaskRestAPI/miniAppsZaKnjizaru/ServerMobilnoBankaPosta/ServerMobilnoBankaPosta.py
+++ TaskRestAPI/miniAppsZaKnjizaru/ServerMobilnoBankaPosta/ServerMobilnoBankaPosta.py
@@ -1,12 +1,10 @@
 import os
 import django
-import os
-import django
 import pickle
-
+import re
 if 'env setting':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.settings')
-    django.setup()
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.settings')
+	django.setup()
 
 from TaskRestAPI.models import *
 from django.utils import timezone
@@ -15,21 +13,25 @@ import json
 from django.db import connection
 from django.contrib.auth import authenticate
 from TaskRestAPI.miniAppsZaKnjizaru.ServerMobilnoBankaPosta.Upiti import *
+
 cursor = connection.cursor()
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
+	userSablon = re.compile(r'\((.*?)\)')
 	def handle(self):
 		data = self.request.recv(10000)
 		print("Poruka od {} :".format(self.client_address[0]))
+
 		try:
 			poruka = str(data.decode())
 		except:
 			poruka = ""
-			bankaIPostaRequest(data,self.request)
+			bankaIPostaRequest(data, self.request)
 
 		if (poruka in upitiZaTab1):
-			if(poruka == "najpKnjige\n"):
+			row = []
+			if (poruka == "najpKnjige\n"):
 				row = cursor.execute(najpKnjige)
 
 			elif (poruka == "besplKnjige\n"):
@@ -40,12 +42,12 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 			elif (poruka == "countNarudzbine\n"):
 				row = cursor.execute(countNarudzbine)
 			# row = cursor.fetchone()
-			sendMessageToClient(row,self.request)
+			sendMessageToClient(row, self.request)
 
 		elif (poruka.startswith("mojeKnjige")):
 			id = poruka.split(" ")[1]
 			print(id)
-			row = cursor.execute(mojeKnjige.replace("nesto",id))
+			row = cursor.execute(mojeKnjige.replace("nesto", id))
 			sendMessageToClient(row, self.request)
 		elif (poruka.startswith("ids")):
 			porukaUDelovima = poruka.split(" ")
@@ -61,69 +63,104 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
 			print(poruka)
 			print(porukaUDelovima)
-			for i in range(1,len(porukaUDelovima)-1):
+			for i in range(1, len(porukaUDelovima) - 1):
 				print(porukaUDelovima[i])
 				obj = StavkeNarudzbine()
 				obj.knjiga = Knjige.objects.get(id=int(porukaUDelovima[i]))
 				obj.narudzbina = narudzbina
 				obj.kolicina = 1
 				obj.save()
-			sendMessageToClient(("OK",),self.request)
+			sendMessageToClient(("OK",), self.request)
 		elif (poruka.startswith("id")):
 			idKnjige = poruka.split(" ")[1]
-			upit = idd.replace("kolona","id").replace("nesto",idKnjige)
+			upit = idd.replace("kolona", "id").replace("nesto", idKnjige)
 			row = cursor.execute(upit)
-			sendMessageToClient(row,self.request)
+			sendMessageToClient(row, self.request)
 		elif (poruka.startswith("komentari")):
 			idKnjige = poruka.split(" ")[1]
-			upit = komentari.replace("nesto",idKnjige)
+			upit = komentari.replace("nesto", idKnjige)
 			row = cursor.execute(upit)
-			sendMessageToClient(row,self.request)
+			sendMessageToClient(row, self.request)
 		elif (poruka.startswith("kategorija")):
-			kategorijaPoruka = "'"+poruka.split("kategorija ")[1].replace("\n","")+"'"
-			upit = kategorija.replace("nesto",kategorijaPoruka)
+			kategorijaPoruka = "'" + poruka.split("kategorija ")[1].replace("\n", "") + "'"
+			upit = kategorija.replace("nesto", kategorijaPoruka)
 			# print(upit)
 			row = cursor.execute(upit)
-			sendMessageToClient(row,self.request)
+			sendMessageToClient(row, self.request)
 		elif (poruka.startswith("getUser")):
 			username = poruka.split(" ")[1]
-			sifra = poruka.split(" ")[2].replace("\n","")
+			sifra = poruka.split(" ")[2].replace("\n", "")
 			user = authenticate(username=username, password=sifra)
-			if(user!= None):
+			if (user != None):
 				print("ok")
 				korisnikObj = Korisnici.objects.get(korisnik_id=user.id)
-				sendMessageToClient([("OK",korisnikObj.id)],self.request)
+				sendMessageToClient([("OK", korisnikObj.id)], self.request)
 				print(korisnikObj.id)
 				korisnikObj = None
 			else:
 				sendMessageToClient([("NOT OK",)], self.request)
 		elif (poruka.startswith("koMentarisi")):
 			userID = poruka.split(" ")[1]
-			komentar = poruka.split
+		# komentar = poruka.split
+		elif (poruka.startswith("reg")):
+
+			print(poruka)
+			podaci = self.userSablon.findall(poruka.replace("\n",""))
+			# reg username email sifra ime prezime ulicaIBroj brojPoste grad
+			try:
+				print(podaci)
+				userObj = User.objects.create_user(username=podaci[0],
+														 email=podaci[1],
+														 password=podaci[2]
+														 )
+				userObj.is_active = True
+				userObj.first_name = podaci[3]
+				userObj.last_name = podaci[4]
+				userObj.save()
+
+				korisnik = Korisnici()
+				korisnik.korisnik = userObj
+				korisnik.ulicaIBroj = podaci[5]
+				korisnik.brojPoste = int(podaci[6])
+				korisnik.grad = podaci[7]
+				korisnik.save()
+
+				sendMessageToClient([("OK", korisnik.id)], self.request)
+
+
+			except Exception as e:
+				print(e)
+				sendMessageToClient([("NOT OK", 0)], self.request)
+
 
 def startServer1():
 	HOST, PORT = "192.168.1.4", 34300
 	server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 	server.serve_forever()
-def sendMessageToClient(row = [],request = ""):
+
+
+def sendMessageToClient(row=[], request=""):
 	lista = createList(row)
 	jsonVar = json.dumps(
 		{"lista": [lista]})
 
-
 	request.sendall(jsonVar.encode())
-def createList(row = []):
+
+
+def createList(row=[]):
 	lista = []
-	if(row != []):
+	if (row != []):
 		for i in row:
 			lista.append(i)
 		return lista
 	else:
 		return ("OK",)
-def bankaIPostaRequest(data,request):
+
+
+def bankaIPostaRequest(data, request):
 	primer = pickle.loads(data)
 	print(primer)
-	if("knjizaraTestNarudzbine" in primer):
+	if ("knjizaraTestNarudzbine" in primer):
 		# izvrsi akciju na bazi
 		for i in primer["knjizaraTestNarudzbine"]:
 			try:
@@ -142,14 +179,14 @@ def bankaIPostaRequest(data,request):
 			try:
 
 				narudzbina = Narudzbine.objects.get(id=i)
-				if(narudzbina.placeno == True):
-					if(narudzbina.datumPrijema == None):
+				if (narudzbina.placeno == True):
+					if (narudzbina.datumPrijema == None):
 						narudzbina.datumPrijema = timezone.now()
 						narudzbina.save()
 						print("narudzbina azurirana")
 			except:
 				continue
-	sendMessageToClient(row = [],request = request)
+	sendMessageToClient(row=[], request=request)
 
 	# from ServerZaMobilno import ServerZaMobilno
-# ServerZaMobilno.startServer1()
+	# ServerZaMobilno.startServer1()
