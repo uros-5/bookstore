b1b4cb355607634830121d4725e1f26dfbeed3d8
uros-5 <uros.mrkobrada99@gmail.com>
2020-09-09 13:21:40 +0100

    refaktorisan deo za kategorije

--- Knjizara/viewsKnjizara/knjigeViews.py
+++ Knjizara/viewsKnjizara/knjigeViews.py
@@ -1,45 +1,18 @@
 from Knjizara.views import *
 from Knjizara.utils import Knjiga
-class Kategorija_look(ListView):
+class Kategorija_view(ListView):
 	model = Korisnici
 	template_name = 'public/knjige/knjige.html'
 	paginate_by = 12
 
 	def get_queryset(self):
-		if (str(self.request.path).startswith("/ljubavni")):
-			knjige_all = Knjige.objects.filter(kategorija="Ljubavni roman")
-			return knjige_all
-		elif (self.request.path == "/istorija/"):
-			knjige_all = Knjige.objects.filter(kategorija="Istorija")
-			return knjige_all
+		self.setKategorija(self.request.path)
+		knjige_all = Knjige.objects.filter(kategorija=self.kategorija)
+		return knjige_all
 
-		elif (self.request.path == "/fantastika/"):
-			knjige_all = Knjige.objects.filter(kategorija="Fantastika")
-			return knjige_all
+	def setKategorija(self,kategorija):
+		self.kategorija = kategorija.replace("/","").capitalize().replace("_"," ")
 
-		elif (self.request.path == "/filozofija/"):
-			knjige_all = Knjige.objects.filter(kategorija="Filozofija")
-			return knjige_all
-
-		elif (self.request.path == "/horor/"):
-			knjige_all = Knjige.objects.filter(kategorija="Horor")
-			return knjige_all
-
-		elif (self.request.path == "/drama/"):
-			knjige_all = Knjige.objects.filter(kategorija="Drama")
-			return knjige_all
-
-		elif (self.request.path.startswith("/autor/")):
-			print(self.request)
-			knjige_all = Knjige.objects.filter(kategorija="Drama")
-			return knjige_all
-
-		elif (self.request.path == "/pretraga/"):
-
-			pretraga = str(self.request.GET.get("pretraga")).upper()
-			knjige_all = Knjige.objects.all().filter(naslov__contains=pretraga)
-			print(knjige_all.count())
-			return knjige_all
 
 
 def knjiga_look(request, ISBN):
