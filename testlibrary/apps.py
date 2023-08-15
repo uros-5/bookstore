7806ae53fcc91c6e67c6fe943f8987a9d74b3d7c
uros <uros.mrkobrada99@gmail.com>
2023-08-15 20:15:11 +0100

    delete all

--- testlibrary/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestlibraryConfig(AppConfig):
-    name = 'testlibrary'
--- testlibrary/tests.py
+++
@@ -1,9 +0,0 @@
-""" from django.test import TestCase """
-
-# Create your tests here.
-
-import re
-sablon = re.compile(r'userInfo\[(.*)\]')
-pretraga = sablon.findall('userInfo[grad]') 
-if pretraga:
-    exec(f'user.{pretraga[0]} = {nesto}')
